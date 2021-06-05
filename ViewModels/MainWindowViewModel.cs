354e6b85efa738c642931353d7ddbd22f0c7ced7
Rukin Danil <rukin2018d@yandex.ru>
2021-06-05 20:03:15 +0700

    Добавьте файлы проекта.

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using MVVMTemplate.ViewModels.Base;
+using System.Windows.Markup;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVVMTemplate.ViewModels
+{
+	[MarkupExtensionReturnType(typeof(MainWindowViewModel))]
+	public class MainWindowViewModel : ViewModel
+	{
+		#region Properties
+		private string _title = "Title";
+		public string Title { get => _title; set => Set(ref _title, value); }
+
+		private string _status = "Status";
+		public string Status { get => _status; set => Set(ref _status, value); }
+		#endregion
+
+		#region Commands
+
+		#endregion
+	}
+}
